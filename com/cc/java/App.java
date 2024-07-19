c4ddc11e0eaa1fc6e46f12ded513cdaa98b2150e
Kafeev95 <andre.ms@web.de>
2024-07-19 12:23:22 +0200

    04 4.Iteration Polymorphie

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -14,29 +14,12 @@ public class App {
 
     }
 
-     // Overloading
-     private static void pollObj(Queen obj){
+     
+     private static void pollObj(HoneyBee obj){
         output(obj.doYourJob());
         output(obj.fly());
         output("----------------");
-    }
-
-
-    // Overloading
-    private static void pollObj(Worker obj){
-        output(obj.doYourJob());
-        output(obj.fly());
-        output("----------------");
-    }
-
-
-    // Overloading
-    private static void pollObj(Drone obj){
-        output(obj.doYourJob());
-        output(obj.fly());
-        output("----------------");
-    }
-
+     }
    
    
     private static void output(String outputStr) {
